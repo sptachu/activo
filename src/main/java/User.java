09d2ccba8b1e63d6e79c29f2fac2e5ba19d6bc84
ptasz <ptaszynskis@student.agh.edu.pl>
2025-01-02 22:36:30 +0100

    more user values and methods but not all of them (tbd)

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,14 +1,70 @@
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class User {
     public boolean ifAdmin;
     public String username;
     public String password;
-    ArrayList<Activity> goals = new ArrayList<Activity>();
+    public String totalActiveTime;
+    public String goalTotalActiveTime;
+    public String timeDifference = timeToGoal(totalActiveTime, goalTotalActiveTime);
+    public String tenKmRunTime;
+    public String goalTenKmRunTime;
+    public  String timeDifferenceRun = timeToGoal(tenKmRunTime, goalTenKmRunTime);
+    public String fortyKmBikeTime;
+    public String goalFortyKmBikeTime;
+    public String timeDifferenceBike = timeToGoal(fortyKmBikeTime, goalFortyKmBikeTime);
+    public String fourHundredMetersSwimTime;
+    public String goalFourHundredMetersSwimTime;
+    public String timeDifferenceSwim = timeToGoal(fourHundredMetersSwimTime, goalFourHundredMetersSwimTime);
+    public int totalDistance;
+    public int goalTotalDistance;
+    public int distanceDifference = goalTotalDistance - totalDistance;
+
     ArrayList<User> friends = new ArrayList<User>();
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+    }
+
+    public static String toFancyTime(long totalSeconds) {
+        Duration duration = Duration.ofSeconds(totalSeconds);
+        LocalTime time = LocalTime.MIDNIGHT.plus(duration); // Start from midnight
+        return time.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+    }
+
+    public static long toSeconds(String timeString) {
+        try {
+            LocalTime time = LocalTime.parse(timeString, DateTimeFormatter.ofPattern("HH:mm:ss"));
+            return time.toSecondOfDay();
+        } catch (DateTimeParseException e) {
+            System.err.println("Niepoprawny format czasu: " + e.getMessage());
+            throw new IllegalArgumentException("Niepoprawne wartości liczbowe w czasie");
+        }
+    }
+
+    public static String timeToGoal(String time, String goalTime) {
+        if(time == null || goalTime == null) {
+            return "No time set";
+        } else {
+            long timeInSeconds = toSeconds(time);
+            long goalTimeInSeconds = toSeconds(goalTime);
+            long timeToGoalInSeconds = goalTimeInSeconds - timeInSeconds;
+            if (timeInSeconds < goalTimeInSeconds) {
+                return "Time is faster than the goal time!";
+            } else if (timeToGoalInSeconds < 60) {
+                return timeToGoalInSeconds + " seconds";
+            } else if (timeToGoalInSeconds < 3600) {
+                long minutes = timeToGoalInSeconds / 60;
+                long seconds = timeToGoalInSeconds % 60;
+                return minutes + "m " + seconds + "s";
+            } else {
+                return toFancyTime(timeToGoalInSeconds);
+            }
+        }
     }
 }
